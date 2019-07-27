ccf900cf8409b72f604000cc247a5ecd8324f4c7
Borgosity <borgosity@gmail.com>
2019-07-27 17:33:01 +1000

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,8 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: env.DATO_API_TOKEN,
+        apiToken: process.env.DATO_API_TOKEN,,
+        previewMode: true,
       },
     },
   ],
