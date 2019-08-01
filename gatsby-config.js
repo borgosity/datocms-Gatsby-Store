b466928db5ea869e8f2d27fe39a24ee7775fc96b
Borgosity <borgosity@gmail.com>
2019-08-01 14:02:43 +1000

    add snipcart test cart

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,15 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: process.env.DATO_API_TOKEN,
         previewMode: true,
       },
+    },
+    {
+      resolve: 'gatsby-plugin-snipcart',
+      options: {
+        apiKey: 'OWE4ZWQ5NjUtMGZmNi00NjM0LWEwYjUtZjIwMjVkY2RjOGNmNjM2OTk2NTQ0NzMzMjE1ODc0',
+        autopop: true
+      }
     },
   ],
 }
